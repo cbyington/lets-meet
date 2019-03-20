9186defd40bbab177d804c2ceb4d9454ae5535c0
Chris Byington <chris.byington@gmail.com>
2019-03-19 21:14:47 -0700

    Update web-auth-workflow.py

--- web-auth-workflow.py
+++ web-auth-workflow.py
@@ -7,13 +7,15 @@ import os
 import flask
 import requests
 import datetime
+import io
 
 import google.oauth2.credentials
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
 
 #CLIENT_SECRETS_FILE = "client_secret_718959481266-rgcep92fv592m6k5fcd1dedu3101f5mm.apps.googleusercontent.com.json"
-CLIENT_SECRETS_FILE = os.environ.get('google_client_secret')
+CLIENT_SECRETS_FILE = io.StringIO(os.environ.get('google_client_secret'))
+#io.StringIO(CLIENT_SECRETS_FILE_RAW)
 
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 API_SERVICE_NAME = 'calendar'
